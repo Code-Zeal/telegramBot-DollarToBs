70eb4bd8c33edc5178389dcb01d71a4c6c669bd9
Code-Zeal <codeseazeal@gmail.com>
2023-12-01 11:58:03 -0400

    test migrate to db

---
+++ bd.js
@@ -0,0 +1,109 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+const DB_USER = process.env.DB_USER;
+const DB_HOST = process.env.DB_HOST;
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_PORT = process.env.DB_PORT;
+
+const pool = new Pool({
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_DATABASE,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+const leerValorDolar = async () => {
+  try {
+    const res = await pool.query(
+      "SELECT bcv, en_paralelo_vzla3, binance_p2p, monitor_dolar_web, en_paralelo_vzla_vip FROM tabla_dolar;"
+    );
+    return res.rows[0];
+  } catch (error) {
+    console.error("Error al obtener datos de la base de datos:", error);
+    return {};
+  }
+};
+const escribirValorDolar = async (valores) => {
+  try {
+    const query = `
+      UPDATE tabla_dolar
+      SET
+        bcv = $1,
+        en_paralelo_vzla3 = $2,
+        binance_p2p = $3,
+        monitor_dolar_web = $4,
+        en_paralelo_vzla_vip = $5
+      WHERE id = 1
+      RETURNING *;
+    `;
+    const values = [
+      valores.bcv,
+      valores.en_paralelo_vzla3,
+      valores.binance_p2p,
+      valores.monitor_dolar_web,
+      valores.en_paralelo_vzla_vip,
+    ];
+    const res = await pool.query(query, values);
+    res.rows[0];
+  } catch (error) {
+    console.error("Error al guardar en la base de datos:", error);
+  }
+};
+// const valores = {
+//   bcv: 35.51,
+//   en_paralelo_vzla3: 37.22,
+//   binance_p2p: 36.92,
+//   monitor_dolar_web: 36,
+//   en_paralelo_vzla_vip: 36,
+// };
+
+// const insertarValoresDolar = async () => {
+//   const query = `
+//     INSERT INTO tabla_dolar(bcv, en_paralelo_vzla3, binance_p2p, monitor_dolar_web, en_paralelo_vzla_vip)
+//     VALUES($1, $2, $3, $4, $5) RETURNING *;
+//   `;
+//   const values = [
+//     valores.bcv,
+//     valores.en_paralelo_vzla3,
+//     valores.binance_p2p,
+//     valores.monitor_dolar_web,
+//     valores.en_paralelo_vzla_vip,
+//   ];
+
+//   try {
+//     const res = await pool.query(query, values);
+//   } catch (error) {
+//     console.error("Error al insertar valores en la base de datos:", error);
+//   }
+// };
+
+// const crearTablaValorDolar = async () => {
+//   try {
+//     const query = `
+//       CREATE TABLE IF NOT EXISTS tabla_dolar (
+//         id SERIAL PRIMARY KEY,
+//         fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+//         bcv DECIMAL(10, 2),
+//         en_paralelo_vzla3 DECIMAL(10, 2),
+//         binance_p2p DECIMAL(10, 2),
+//         monitor_dolar_web DECIMAL(10, 2),
+//         en_paralelo_vzla_vip DECIMAL(10, 2)
+//       );
+//     `;
+
+//     await pool.query(query);
+//   } catch (error) {
+//     console.error("Error al crear la tabla 'tabla_dolar':", error);
+//   }
+// };
+
+module.exports = {
+  leerValorDolar,
+  escribirValorDolar,
+  insertarValoresDolar,
+  crearTablaValorDolar,
+};
--- index.js
+++ index.js
@@ -1,8 +1,48 @@
-const { getPuppeteerValues } = require("./puppeteerConfig");
-const { fileOperations } = require("./fileOperations");
+require("dotenv").config();
 const { getDayOfWeek, getHour } = require("./hourAndDate");
-const { sendMessage, sendError } = require("./telegramConfig");
+const { leerValorDolar, escribirValorDolar } = require("./bd");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const { executablePath } = require("puppeteer");
+puppeteer.use(StealthPlugin());
+const ID_MARCE = process.env.ID_MARCE;
+const ID_JAHN = process.env.ID_JAHN;
+const ID_DANIRIS = process.env.ID_DANIRIS;
+const ID_JHONI = process.env.ID_JHONI;
+const ID_GUSTAVO = process.env.ID_GUSTAVO;
+const ID_YEINY = process.env.ID_YEINY;
+const TELEGRAM_TOKEN_BCV = process.env.TELEGRAM_TOKEN_BCV;
+const telegramBot = require("node-telegram-bot-api");
+const bot_bcv = new telegramBot(TELEGRAM_TOKEN_BCV, { polling: true });
 
+const sendMessage = async (message) => {
+  try {
+    const chatIds = [
+      ID_MARCE,
+      ID_JAHN,
+      ID_DANIRIS,
+      ID_GUSTAVO,
+      ID_JHONI,
+      ID_YEINY,
+    ];
+    chatIds.forEach((chatId) => {
+      bot_bcv.sendMessage(chatId, message);
+    });
+  } catch (error) {
+    bot_bcv.sendMessage(ID_MARCE, error);
+  }
+};
+const sendError = async (err) => {
+  try {
+    bot_bcv.sendMessage(ID_MARCE, err);
+  } catch (err) {
+    bot_bcv.sendMessage(ID_MARCE, err);
+  }
+};
+const BASE_SELECTOR =
+  "#root > div > div.grid.grid-cols-12 > div.lg\\:col-span-8.sm\\:col-span-12.col-span-12.mx-1.overflow-x-auto.px-5 > table > tbody > tr:nth-child";
+const TIMEOUT = { waitUntil: "networkidle0", timeout: 90000 };
+const URL = process.env.URL;
 const main = async () => {
   try {
     const dayOfWeek = getDayOfWeek();
@@ -17,13 +57,126 @@ const main = async () => {
       console.log("hora de descanso");
       return;
     }
-    const valorActualDolar = await getPuppeteerValues();
+    const browser = await puppeteer.launch({
+      headless: true,
+      slowMo: 0,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : executablePath(),
+    });
+    const page = await browser.newPage();
+    await page.goto(URL, TIMEOUT);
 
-    const message = fileOperations(valorActualDolar);
+    const valorActualDolar = await page.evaluate((selectorStr) => {
+      const getValue = (index) => {
+        return document
+          .querySelector(`${selectorStr}(${index}) > td:nth-child(2)`)
+          .textContent.trim()
+          .slice(0, 5);
+      };
+
+      const bcv = parseFloat(getValue(1).replace(",", "."));
+      const en_paralelo_vzla3 = parseFloat(getValue(2).replace(",", "."));
+      const binance_p2p = parseFloat(getValue(3).replace(",", "."));
+      const monitor_dolar_web = parseFloat(getValue(4).replace(",", "."));
+      const en_paralelo_vzla_vip = parseFloat(getValue(5).replace(",", "."));
+
+      return {
+        bcv,
+        en_paralelo_vzla3,
+        binance_p2p,
+        monitor_dolar_web,
+        en_paralelo_vzla_vip,
+      };
+    }, BASE_SELECTOR);
+
+    await browser.close();
+
+    const calcularCambioPorcentual = (anterior, actual) => {
+      if (!Number(anterior)) return 0;
+      if (Number(actual) === Number(anterior)) return 0;
+      return ((Number(actual) - Number(anterior)) / Number(anterior)) * 100;
+    };
+    const valorAnteriorDolar = await leerValorDolar();
+    const cambioPorcentual = {};
+    for (const key in valorActualDolar) {
+      const actual = valorActualDolar[key];
+      const anterior = parseFloat(valorAnteriorDolar[key]);
+      cambioPorcentual[key] = calcularCambioPorcentual(anterior, actual);
+    }
+
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleString("es-VE", {
+      timeZone: "America/Caracas",
+    });
+    const mensajesCambio = {};
+
+    for (const key in valorActualDolar) {
+      const actual = valorActualDolar[key];
+      const anterior = parseFloat(valorAnteriorDolar[key]);
+      cambioPorcentual[key] = calcularCambioPorcentual(anterior, actual);
+      if (cambioPorcentual[key] > 0) {
+        mensajesCambio[key] = `📈 ${cambioPorcentual[key].toFixed(2)}%`;
+      } else if (cambioPorcentual[key] < 0) {
+        mensajesCambio[key] = `📉 ${Math.abs(cambioPorcentual[key]).toFixed(
+          2
+        )}%`;
+      } else {
+        mensajesCambio[key] = "";
+      }
+    }
+
+    const status =
+      hour + 1 > 18 && dayOfWeek + 1 !== 6
+        ? "🔴🔴🔴CIERRE DEL DÍA🔴🔴🔴"
+        : hour + 1 > 18 && dayOfWeek + 1 === 6
+        ? "🔴🔴CIERRE DE LA SEMANA🔴🔴"
+        : hour - 1 < 7 && dayOfWeek - 1 !== 0
+        ? "🟢🟢🟢INICIO DEL DÍA🟢🟢🟢"
+        : hour - 1 < 7 && dayOfWeek - 1 === 0
+        ? "🟢🟢🟢INICIO DE LA SEMANA🟢🟢🟢"
+        : "";
+    const message = `
+    ${status}
+    Fecha: ${formattedDate}\n
+    Cambios del dolar a Bs\n
+    🔵BCV:${valorActualDolar.bcv} ${
+      mensajesCambio["bcv"] === undefined ? "" : mensajesCambio["bcv"]
+    }\n
+    🟡ParaleloVzla3:${valorActualDolar.en_paralelo_vzla3} ${
+      mensajesCambio["en_paralelo_vzla3"] === undefined
+        ? ""
+        : mensajesCambio["en_paralelo_vzla3"]
+    }\n
+    🔴MonitorDolarWeb:${valorActualDolar.monitor_dolar_web} ${
+      mensajesCambio["monitor_dolar_web"] === undefined
+        ? ""
+        : mensajesCambio["monitor_dolar_web"]
+    }\n
+      🟡ParaleloVzlaVip:${valorActualDolar.en_paralelo_vzla_vip} ${
+      mensajesCambio["en_paralelo_vzla_vip"] === undefined
+        ? ""
+        : mensajesCambio["en_paralelo_vzla_vip"]
+    }\n
+    🔶BinanceP2P:${valorActualDolar.binance_p2p} ${
+      mensajesCambio["binance_p2p"] === undefined
+        ? ""
+        : mensajesCambio["binance_p2p"]
+    }
+     `;
+    await escribirValorDolar(valorActualDolar);
     sendMessage(message);
   } catch (error) {
     sendError("error en main()");
     console.error(error);
+    main();
   }
 };
 
