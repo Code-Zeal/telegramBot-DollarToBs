33efc001396a9350854c53a913766bd6fe11f71e
Code-Zeal <codeseazeal@gmail.com>
2023-12-04 13:33:22 -0400

    fixing express

--- api.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-app.get("/health", (req, res) => {
-  res.sendStatus(200);
-});
-
-module.exports = router;
--- index.js
+++ index.js
@@ -6,19 +6,16 @@ const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const { executablePath } = require("puppeteer");
 puppeteer.use(StealthPlugin());
 const ID_MARCE = process.env.ID_MARCE;
-const ID_JAHN = process.env.ID_JAHN;
-const ID_DANIRIS = process.env.ID_DANIRIS;
-const ID_JHONI = process.env.ID_JHONI;
-const ID_GUSTAVO = process.env.ID_GUSTAVO;
-const ID_YEINY = process.env.ID_YEINY;
 const TELEGRAM_TOKEN_BCV = process.env.TELEGRAM_TOKEN_BCV;
 const telegramBot = require("node-telegram-bot-api");
 const bot_bcv = new telegramBot(TELEGRAM_TOKEN_BCV, { polling: true });
 const port = process.env.PORT || 8080;
 const express = require("express");
 const app = express();
-const apiRouter = require("./api");
-app.use("/api", apiRouter);
+app.get("/health", (req, res) => {
+  res.sendStatus(200);
+});
+
 const sendMessage = async (message) => {
   try {
     const chatIds = [ID_MARCE];
