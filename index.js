27219c90a2a11072ab792a0bf4dde33a7dba9d43
Code-Zeal <codeseazeal@gmail.com>
2023-12-04 14:10:42 -0400

    all chatId

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const { executablePath } = require("puppeteer");
 puppeteer.use(StealthPlugin());
 const ID_MARCE = process.env.ID_MARCE;
+const ID_JAHN = process.env.ID_JAHN;
+const ID_DANIRIS = process.env.ID_DANIRIS;
+const ID_JHONI = process.env.ID_JHONI;
+const ID_GUSTAVO = process.env.ID_GUSTAVO;
+const ID_YEINY = process.env.ID_YEINY;
 const TELEGRAM_TOKEN_BCV = process.env.TELEGRAM_TOKEN_BCV;
 const telegramBot = require("node-telegram-bot-api");
 const bot_bcv = new telegramBot(TELEGRAM_TOKEN_BCV, { polling: true });
@@ -18,7 +23,14 @@ app.get("/health", (req, res) => {
 
 const sendMessage = async (message) => {
   try {
-    const chatIds = [ID_MARCE];
+    const chatIds = [
+      ID_MARCE,
+      ID_JAHN,
+      ID_DANIRIS,
+      ID_GUSTAVO,
+      ID_JHONI,
+      ID_YEINY,
+    ];
     chatIds.forEach((chatId) => {
       bot_bcv.sendMessage(chatId, message);
     });
